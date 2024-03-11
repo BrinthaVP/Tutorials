da64326ca9690aa44e54f372c5e7973611952a92
BrinthaVP <51736151+BrinthaVP@users.noreply.github.com>
2024-03-12 04:53:17 +0530

    Update alignment.sh

--- alignment.sh
+++ alignment.sh
@@ -6,3 +6,6 @@ bwa mem -Y -R '@RG\tID:'${1}'\tLB:'${1}'\tPL:ILLUMINA\tPM:HISEQ\tSM:'${1} /data/
 export -f do_one
 
 cat "file_name" | parallel -P 1 do_one {}
+
+
+do_one() { echo ${1}; java -jar ~/LVM_Multistrain/Preprocessing/gatk-4.1.8.0/gatk-package-4.1.8.0-local.jar MarkDuplicatesSpark -I ${1}.sam -M dedup_metrics.txt -O ${1}.bam --create-output-bam-index true --spark-master local[10];  }
